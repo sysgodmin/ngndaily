19b7106e6bbbe185cb1f06f96a3a5c3ad6453aaf
brandon <brandon@ngndaily.(none)>
2015-02-21 10:48:19 -0500

    fixed css: alignment/sizing issues in bootstrap.override.css

--- public/javascripts/btn_func.js
+++
@@ -1,8 +0,0 @@
-(function (window, $, document) {
-	var login = $("#login");
-	login.click(function (e){
-		events.alert();
-		e.preventDefault();
-		return false;
-	})
-}(typeof window !== 'undefined' ? window : this, jQuery, document))
--- public/javascripts/form_veri.js
+++
@@ -1,1 +0,0 @@
-!function(e,f){var a=$("#article-form"),b=$("#title"),c=$("#article"),d=$("#meta");a.unbind("submit").bind("submit",function(a){if(2<b.val().length&&100<c.val().length&&1<d.val().length)this.submit();else return a.preventDefault(),!1})}("undefined"!==typeof window?window:this,document);
